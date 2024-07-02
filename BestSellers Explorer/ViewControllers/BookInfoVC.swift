40be81c29daa46e322059bb71eaedf7d54ead290
SnowwLeopardd <112858225+SnowwLeopardd@users.noreply.github.com>
2024-07-01 14:38:35 -1000

    fix UI of BookInfoVC

--- BestSellers Explorer/ViewControllers/BookInfoVC.swift
+++ BestSellers Explorer/ViewControllers/BookInfoVC.swift
@@ -12,13 +12,14 @@ class BookInfoVC: UIViewController {
     private var book: Book
     
     internal let bookImage = UIImageView()
+    private let bookImageContainer = UIView()
     
     private let bookName = UILabel()
     private let authorLabel = UILabel()
     private let bookDescription = UILabel()
     
     private let exploreAgain = UIButton()
-    private let addTofavorites = UIButton()
+    private let addToFavorites = UIButton()
     
     internal let coreDataManager: CoreDataManagerProtocol = CoreDataManager()
     
@@ -37,47 +38,77 @@ class BookInfoVC: UIViewController {
     }
     
     private func setupUI() {
-        view.backgroundColor = .white
+        view.backgroundColor = #colorLiteral(red: 0.9610984921, green: 0.9610984921, blue: 0.9610984921, alpha: 1)
+        
         fetchBookImage(from: book)
-        setupBookImage()
-        setupBookName()
+        
+        setupBookImageContainer()
+        setupBookImageView()
+        setupBookNameLabel()
         setupAuthorLabel()
-        setupBookDescription()
-        setupAddToFavoritesUI()
-        setupExploreAgainUI()
+        setupBookDescriptionLabel()
+        setupAddToFavoritesButton()
+        setupExploreAgainButton()
     }
     
     // MARK: - SetupUI
-    private func setupBookImage() {
-        view.addSubview(bookImage)
+    private func setupBookImageContainer() {
+        bookImageContainer.layer.shadowColor = UIColor.black.cgColor
+        bookImageContainer.layer.shadowOpacity = 0.5
+        bookImageContainer.layer.shadowOffset = CGSize(width: 0, height: 2)
+        bookImageContainer.layer.shadowRadius = 4
+        bookImageContainer.layer.masksToBounds = false
+        
+        view.addSubview(bookImageContainer)
+        
+        bookImageContainer.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            bookImageContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            bookImageContainer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            bookImageContainer.heightAnchor.constraint(equalToConstant: 340),
+            bookImageContainer.widthAnchor.constraint(equalToConstant: 270),
+        ])
+    }
+    
+    private func setupBookImageView() {
+        bookImage.layer.cornerRadius = 10
+        bookImage.clipsToBounds = true
+        
+        bookImageContainer.addSubview(bookImage)
         
         bookImage.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            bookImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            bookImage.bottomAnchor.constraint(equalTo: view.topAnchor, constant: 400),
-            bookImage.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            bookImage.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+            bookImage.topAnchor.constraint(equalTo: bookImageContainer.topAnchor),
+            bookImage.leadingAnchor.constraint(equalTo: bookImageContainer.leadingAnchor),
+            bookImage.trailingAnchor.constraint(equalTo: bookImageContainer.trailingAnchor),
+            bookImage.bottomAnchor.constraint(equalTo: bookImageContainer.bottomAnchor)
         ])
     }
     
-    private func setupBookName() {
+    private func setupBookNameLabel() {
         bookName.text = book.title
+        bookName.textAlignment = .center
+        bookName.numberOfLines = 2
+        bookName.font = UIFont.boldSystemFont(ofSize: 19)
         
         view.addSubview(bookName)
         
         bookName.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            bookName.topAnchor.constraint(equalTo: bookImage.bottomAnchor),
-            //            bookName.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
-            bookName.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            bookName.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+            bookName.topAnchor.constraint(equalTo: bookImage.bottomAnchor, constant: 16),
+            bookName.trailingAnchor.constraint(equalTo: bookImage.trailingAnchor),
+            bookName.leadingAnchor.constraint(equalTo: bookImage.leadingAnchor)
         ])
     }
     
     private func setupAuthorLabel() {
         authorLabel.text = book.author
+        authorLabel.textAlignment = .center
+        authorLabel.numberOfLines = 2
+        authorLabel.textColor = .gray
         
         view.addSubview(authorLabel)
         
@@ -85,80 +116,79 @@ class BookInfoVC: UIViewController {
         
         NSLayoutConstraint.activate([
             authorLabel.topAnchor.constraint(equalTo: bookName.bottomAnchor),
-            authorLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            authorLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+            authorLabel.trailingAnchor.constraint(equalTo: bookImage.trailingAnchor),
+            authorLabel.leadingAnchor.constraint(equalTo: bookImage.leadingAnchor)
         ])
     }
     
-    private func setupBookDescription() {
+    private func setupBookDescriptionLabel() {
         let noDescription = "API doesn't provide description for books in this category"
         bookDescription.text = book.description.isEmpty ? noDescription : book.description
         bookDescription.numberOfLines = 10
+        bookDescription.textAlignment = .justified
         
         view.addSubview(bookDescription)
         
         bookDescription.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            bookDescription.topAnchor.constraint(equalTo: authorLabel.bottomAnchor),
-            bookDescription.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            bookDescription.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            bookDescription.topAnchor.constraint(equalTo: authorLabel.bottomAnchor, constant: 16),
+            bookDescription.trailingAnchor.constraint(equalTo: bookImage.trailingAnchor),
+            bookDescription.leadingAnchor.constraint(equalTo: bookImage.leadingAnchor)
         ])
     }
     
-    
-    private func setupAddToFavoritesUI() {
-        addTofavorites.setTitle("Add to Favorites", for: .normal)
-        addTofavorites.backgroundColor = UIColor.blue
-        addTofavorites.setTitleColor(UIColor.white, for: .normal)
-        addTofavorites.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
-        addTofavorites.layer.cornerRadius = 10
-        addTofavorites.addTarget(self, action: #selector(setupAddToFavoriesLogic), for: .touchUpInside)
+    private func setupAddToFavoritesButton() {
+        addToFavorites.setTitle("Add to Favorites", for: .normal)
+        addToFavorites.backgroundColor = UIColor.black
+        addToFavorites.setTitleColor(UIColor.white, for: .normal)
+        addToFavorites.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        addToFavorites.layer.cornerRadius = 10
+        addToFavorites.addTarget(self, action: #selector(setupAddToFavoritesLogic), for: .touchUpInside)
         
-        view.addSubview(addTofavorites)
+        view.addSubview(addToFavorites)
         
-        addTofavorites.translatesAutoresizingMaskIntoConstraints = false
+        addToFavorites.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            addTofavorites.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            addTofavorites.bottomAnchor.constraint(equalTo: bookDescription.bottomAnchor, constant: 100),
-            addTofavorites.widthAnchor.constraint(equalToConstant: 200),
-            addTofavorites.heightAnchor.constraint(equalToConstant: 40)
+            addToFavorites.topAnchor.constraint(equalTo: bookDescription.bottomAnchor, constant: 16),
+            addToFavorites.trailingAnchor.constraint(equalTo: bookImage.trailingAnchor),
+            addToFavorites.leadingAnchor.constraint(equalTo: bookImage.leadingAnchor)
         ])
     }
     
-    private func setupExploreAgainUI() {
+    private func setupExploreAgainButton() {
         exploreAgain.setTitle("Explore again", for: .normal)
-        exploreAgain.backgroundColor = UIColor.blue
+        exploreAgain.backgroundColor = UIColor.black
         exploreAgain.setTitleColor(UIColor.white, for: .normal)
         exploreAgain.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         exploreAgain.layer.cornerRadius = 10
-        exploreAgain.addTarget(self, action: #selector(setupExploreAgainLogic), for: .touchUpInside)
+        exploreAgain.addTarget(self, action: #selector(exploreAgainButtonTapped), for: .touchUpInside)
         
         view.addSubview(exploreAgain)
     
         exploreAgain.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            exploreAgain.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            exploreAgain.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
-            exploreAgain.widthAnchor.constraint(equalToConstant: 200),
-            exploreAgain.heightAnchor.constraint(equalToConstant: 40)
+            exploreAgain.topAnchor.constraint(equalTo: addToFavorites.bottomAnchor, constant: 16),
+            exploreAgain.trailingAnchor.constraint(equalTo: bookImage.trailingAnchor),
+            exploreAgain.leadingAnchor.constraint(equalTo: bookImage.leadingAnchor),
+            exploreAgain.bottomAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16)
         ])
     }
     
     // MARK: - ButtonsLogic
-    @objc private func setupAddToFavoriesLogic() {
+    @objc private func setupAddToFavoritesLogic() {
         let isUnique = coreDataManager.isUnique(book.primaryIsbn13)
         if isUnique {
             coreDataManager.create(book)
         } else {
-            AlertController.showErrorAlert(on: self, 
+            AlertController.showErrorAlert(on: self,
                                            message: "This book has already been added to your favorites.")
         }
     }
     
-    @objc func setupExploreAgainLogic() {
+    @objc private func exploreAgainButtonTapped() {
         navigationController?.pushViewController(CalendarVC(), animated: true)
     }
 }
